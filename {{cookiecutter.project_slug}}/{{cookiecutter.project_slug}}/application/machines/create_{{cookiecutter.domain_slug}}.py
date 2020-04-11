3d8511cf3811778fd35abf2a8f6bdf664e03fdfa
Chris Maillefaud <christian.maillefaud@noverde.com.br>
2020-04-11 18:36:57 -0300

    feat: Add Step Functions example

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/application/machines/create_{{cookiecutter.domain_slug}}.py
@@ -0,0 +1,9 @@
+from loguru import logger
+
+
+def find_{{cookiecutter.domain_slug}}_instance(event, context):
+    """Very simple step.
+
+    Step Functions example - WIP
+    """
+    logger.debug("Start step...")
