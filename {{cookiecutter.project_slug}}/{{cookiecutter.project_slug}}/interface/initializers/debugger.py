f13f67abd626d62bf5c9174a96021462ba4e72aa
Danilo Ribeiro <danilo.ribeiro@noverde.com.br>
2020-05-19 19:26:20 -0300

    fix black format

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/interface/initializers/debugger.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/interface/initializers/debugger.py
@@ -1,11 +1,13 @@
 import os
 from loguru import logger
 
+
 def initialize_debug() -> None:
-    if  os.getenv("DEBUG", "false").lower() == 'true':
+    if os.getenv("DEBUG", "false").lower() == "true":
         import ptvsd
+
         logger.info(
             "-------------------------------------------------> WAITING CONNECTION ON 0.0.0.0:5890 TO DEBUG "
         )
-        ptvsd.enable_attach(address=('0.0.0.0', 5890), redirect_output=True)
+        ptvsd.enable_attach(address=("0.0.0.0", 5890), redirect_output=True)
         ptvsd.wait_for_attach()
