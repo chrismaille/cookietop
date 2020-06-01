fe543f17f0692781e247bb34e14cea996cb787e5
Chris Maillefaud <chrismaillefaud@gmail.com>
2020-06-01 09:00:40 -0300

    feat: Deploy automático via Github Actions
    ### Changes
    * Add new job in Github Actions
    
    WIP SER-188

---
+++ {{cookiecutter.project_slug}}/release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    branch: 'master',
+    repositoryUrl: "https://github.com/noverde/{{cookiecutter.project_slug}}",
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/github'
+    ]
+}
