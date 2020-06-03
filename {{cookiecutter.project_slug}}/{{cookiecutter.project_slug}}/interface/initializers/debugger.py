5745b7f9779bfeac6af8999e11f98d58f19490f1
Chris Maillefaud <chrismaillefaud@gmail.com>
2020-06-03 13:53:19 -0300

    fix: tests

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/interface/initializers/debugger.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/interface/initializers/debugger.py
@@ -7,7 +7,8 @@ def initialize_debug() -> None:
         import ptvsd
 
         logger.info(
-            "-------------------------------------------------> WAITING CONNECTION ON 0.0.0.0:5890 TO DEBUG "
+            "-------------------------------------------------> "
+            "WAITING CONNECTION ON 0.0.0.0:5890 TO DEBUG "
         )
         ptvsd.enable_attach(address=("0.0.0.0", 5890), redirect_output=True)
         ptvsd.wait_for_attach()
