272f1e7f8c1a6d39110dbec7bb421586f49579b6
Chris Maillefaud <chrismaillefaud@gmail.com>
2021-05-12 12:58:19 -0300

    feat: Streamlining modules structure
    BREAKING CHANGE: This is a major version. Changes:
    
    * Streamlining modules structure.
    * Remove Python 3.6 support
    * Add Python 3.8 support
    * Add VSCode Remote Development support
    * Add Pycharm Run/Debug configuration

---
+++ {{cookiecutter.project_name_slug}}/{{cookiecutter.project_name_slug}}/handlers/authorize_{{cookiecutter.model_name_slug}}.py
@@ -0,0 +1,22 @@
+from interface.aws.handler_view import view
+from interface.aws.request import Request
+from interface.aws.policy_generator import PolicyGenerator, Effect
+from helpers.handler_response import HandlerResponse
+from typing import Any
+
+
+@view()
+def authorize(request: Request, **kwargs: Any) -> HandlerResponse:
+    """Lambda Request Authorizer.
+
+    Add here your Authorization logics.
+
+    Default code will always allow requests.
+
+    """
+    token = request.aws_event.get("authorizationToken")
+    method_arn = request.aws_event.get("methodArn")
+
+    policy = PolicyGenerator(principal_id="user", resource=method_arn)
+    return policy.build(Effect.ALLOW)
+
